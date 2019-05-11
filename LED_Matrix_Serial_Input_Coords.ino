0edeebbfe54e0e07de5ab11e90ee8aab3828d927
MatthewHaahr19 <50528306+MatthewHaahr19@users.noreply.github.com>
2019-05-11 12:01:52 -0700

    Update LED_Matrix_Serial_Input_Coords.ino

--- LED_Matrix_Serial_Input_Coords.ino
+++ LED_Matrix_Serial_Input_Coords.ino
@@ -32,53 +32,53 @@ void setup() {
 }
 
 void drawDot(int xval,int yval, int state){
-    int x = xval;
-    int y = yval;
-    int panel = x / 8;
-    int ax = 7 - x%8;
+    int x = xval; //sets the x position that the dot appears on
+    int y = yval; //sets the y position that the dot appears on
+    int panel = x / 8; // sets the panel that the dot appears on
+    int ax = 7 - x%8; // sets the adjusted x value of the dot due to the panels changing the coordinates
        
-    lc.setLed(panel,y,ax,state);
+    lc.setLed(panel,y,ax,state); // sets the LED the coordinate y, ax on the specified panel to the state
     Serial.print("X Value: ");
-    Serial.print(x);
+    Serial.print(x); //prints overall x position
     Serial.print("\t Y Value: ");
-    Serial.print(y);
+    Serial.print(y); //prints overall y position
     Serial.print("\t Adjusted X Value: ");
-    Serial.print(ax);
+    Serial.print(ax); //prints adjust x position
     Serial.print("\t Panel Number: ");
-    Serial.println(panel);
+    Serial.println(panel); //prints panel number
 }
 
 
 void loop(){
   if (Serial.available() > 0) {
-    int incomingByte = Serial.read();
-    if (('0' <= incomingByte) && (incomingByte <= '9')){
-      int digit = (incomingByte) - '0';
-      dig = dig +1; 
-      Serial.print("Dig: ");
-      Serial.print(dig);
+    int incomingByte = Serial.read(); //reads the incoming serial data
+    if (('0' <= incomingByte) && (incomingByte <= '9')){ //makes sure that the incoming data is a digit
+      int digit = (incomingByte) - '0';  //sets the digit to read the first digit
+      dig = dig +1; //advances the digit by one
+      Serial.print("Dig: "); 
+      Serial.print(dig); //prints which digit it is
       Serial.print("\t Read from Serial: ");
-      Serial.println(incomingByte);
-      if(dig == 1){
-        xval += digit * 10;
+      Serial.println(incomingByte); //prints what the arduino recieves from the serial
+      if(dig == 1){ //checks if it is on the first digit (10s position for the x coordinate)
+        xval += digit * 10; //sets the read value equal to the 10s position of the x value
       }
       
-      if(dig == 2){
-        xval += digit * 1;
+      if(dig == 2){ //checks if it is on the second digit (1s position for the x coordinate)
+        xval += digit * 1; //sets the read value equal to the 1s position of the x value
       }
-      if(dig == 3){
-        yval += digit * 10;
+      if(dig == 3){ //checks if it is on the third digit (10s position for the y coordinate)
+        yval += digit * 10; //sets the read value equal to the 10s position of the y value
       }
       
-      if(dig >= 4){
-        yval += digit * 1;
-        dig = 0;
-        drawDot(xvalold,yvalold, 0);
-        drawDot(xval,yval, 1);
-        xvalold=xval;
-        yvalold=yval;
-        xval=0;
-        yval=0;
+      if(dig >= 4){ //checks if it is on the fourth digit (1s position for the y coordinate)
+        yval += digit * 1; //sets the read value equal to the 1s position of the y value
+        dig = 0; //resets digit count to 0
+        drawDot(xvalold,yvalold, 0); //turns off LED position
+        drawDot(xval,yval, 1); //turns on new LED position
+        xvalold=xval; //sets the old x value to equal the new
+        yvalold=yval; //sets the old y value to equal the new
+        xval=0; //resets x value
+        yval=0; //resets y value
       }
       
     } 
